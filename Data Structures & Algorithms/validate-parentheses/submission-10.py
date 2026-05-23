4126be91ced50c2db445ed3532f56c3b23f355fd
Tyler Nguyen <tyler.nguyen1688@gmail.com>
2026-05-22 21:50:26 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashMap = {']': '[', ')':'(','}':'{'}
+        for c in s:
+            if c in hashMap:
+                if not stack or stack.pop() != hashMap[c]:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
