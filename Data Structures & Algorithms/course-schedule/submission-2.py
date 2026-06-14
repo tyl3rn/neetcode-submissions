68fc518052431d1cf3ec3c686087271a070990d3
Tyler Nguyen <tyler.nguyen1688@gmail.com>
2026-06-14 15:29:44 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        for u, v in prerequisites:
+            adjList[u].append(v)
+        def dfs(node, path):
+                if node in path:
+                    return False
+                if adjList[node] == []:
+                    return True
+                path.add(node)
+                for value in adjList[node]:
+                    if not dfs(value,path):
+                        return False
+                path.remove(node)
+                return True
+                
+
+        for i in range(numCourses):
+            if not dfs(i, set()):
+                return False
+        return True
+
+            
+        
+        
+        
