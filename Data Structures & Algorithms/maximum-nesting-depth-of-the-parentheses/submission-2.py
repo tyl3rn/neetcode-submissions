40268d13852861e6c0d5ac0850edd3329b83a66c
Tyler Nguyen <tyler.nguyen1688@gmail.com>
2026-06-21 15:59:08 -0400

    Add: maximum-nesting-depth-of-the-parentheses - submission-2

---
+++ Data Structures & Algorithms/maximum-nesting-depth-of-the-parentheses/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        res = 0
+        curRes = 0
+        for char in s:
+            if char == "(":
+                curRes +=1
+                res = max(curRes, res)
+            elif char == ")":
+                    curRes -= 1    
+        return res
